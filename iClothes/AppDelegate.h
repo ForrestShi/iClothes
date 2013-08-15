2878ef70117c07b63ec6e95d542fa52ff1ac5d5f
Forrest <forrest.shi@gmail.com>
2013-08-15 15:04:12 +0800

    Initial Commit

---
+++ iClothes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iClothes
+//
+//  Created by Shi Lin on 8/15/13.
+//  Copyright (c) 2013 Forrest Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
